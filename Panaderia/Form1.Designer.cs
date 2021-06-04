974904fcd3fec29981136b39c3213cd0aaa787c8
Levin Perez <levin1200@gmail.com>
2021-06-03 18:08:53 -0600

    Junio 03 6:08 pm

--- Panaderia/Form1.Designer.cs
+++ Panaderia/Form1.Designer.cs
@@ -83,7 +83,7 @@ namespace Panaderia
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(821, 24);
+            this.panel1.Location = new System.Drawing.Point(819, 61);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(125, 132);
             this.panel1.TabIndex = 154;
